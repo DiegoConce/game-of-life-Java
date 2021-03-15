6dcbdd958cbff4f19b7a0f99ae5d296d79e695f8
DiegoConce <diegocoonce@gmail.com>
2021-03-15 17:41:35 +0100

    init tests

---
+++ src/test/java/it/unicam/cs/pa/jlife104936/a.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.pa.jlife104936;
+
+public class a {
+}
---
+++ src/test/java/it/unicam/cs/pa/jlife104936/controller/ControllerTest.java
@@ -0,0 +1,34 @@
+package it.unicam.cs.pa.jlife104936.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    Controller ctrTest = new Controller();
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void applyRules() {
+    }
+
+    @Test
+    void clearBoard() {
+    }
+
+    @Test
+    void randomBoard() {
+    }
+
+    @Test
+    void saveBoard() {
+    }
+
+    @Test
+    void loadBoard() {
+    }
+}
